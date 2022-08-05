a47af8cc619828f0c8b1df60690a69bbc5f05424
michaelfoz <101305696+michaelfoz@users.noreply.github.com>
2022-08-05 15:08:57 -0700

    Add files via upload

---
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -0,0 +1,40 @@
+/* 
+    This file (cities.js) created per Module 13.4.2: Map Multiple Points
+        Link: https://courses.bootcampspot.com/courses/1225/pages/13-dot-4-2-map-multiple-points?module_item_id=499172
+
+    This file was created to house large dataset--in this case, each city.
+
+*/
+
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
